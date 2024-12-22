2f3456c531fffd7f5d12bf7b50507e4551b02758
SacredLucy <ignirolon@gmail.com>
2024-12-22 18:41:00 -0300

    Updated general error handling

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,31 +1,31 @@
 module.exports = {
     name: 'interactionCreate',
     async execute(interaction, client, race) {
-        if (interaction.isChatInputCommand()) {
-            if (!client.commands.has(interaction.commandName)) return;
-            try {
-                await client.commands.get(interaction.commandName).execute(interaction, client, race);
-            } catch (error) {
-                console.error(error);
-                try{
-                    if(!interaction.replied){
-                        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-                    }else{
+        try{
+            if (interaction.isChatInputCommand()) {
+                if (!client.commands.has(interaction.commandName)) return;
+                try {
+                    await client.commands.get(interaction.commandName).execute(interaction, client, race);
+                } catch (error) {
+                    console.error(error);
+                    if(interaction.replied || interaction.deferred){
                         await interaction.editReply({ content: 'There was an error while executing this command!'});
+                    }else{
+                        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
                     }
-                }catch(error){
+                }
+            }
+
+            if (interaction.isButton()) {
+                if (!client.buttons.has(interaction.customId)) return;
+                try {
+                    await client.buttons.get(interaction.customId).execute(interaction, client, race);
+                } catch (error) {
                     console.error(error);
                 }
             }
-        }
-
-        if (interaction.isButton()) {
-            if (!client.buttons.has(interaction.customId)) return;
-            try {
-                await client.buttons.get(interaction.customId).execute(interaction, client, race);
-            } catch (error) {
-                console.error(error);
-            }
+        }catch(error){
+            console.error(error);
         }
     }
 };
